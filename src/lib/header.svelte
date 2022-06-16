b22590e67b48bcd739f27d0ec7774d99e87e8400
conlinm <conlinm@me.com>
2022-06-15 17:15:28 -0700

    additional pages for routing

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,28 +1,36 @@
 <header>
     <div class="navbar bg-base-100">
         <div class="flex-none">
-          <button class="btn btn-square btn-ghost">
+          <div class="dropdown dropdown-right">
+          <label tabindex="0" for="" class="btn btn-square btn-ghost">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </button>
+          </label>
+          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+    <li><a>Item 1</a></li>
+    <li><a>Item 2</a></li>
+  </ul>
         </div>
+      </div>
         <div class="flex-1">
-          <a class="btn btn-ghost normal-case text-xl">myProstate</a>
+          <a class="btn btn-ghost normal-case text-xl" href="/">myProstate</a>
         </div>
         <div class="dropdown dropdown-end">
-      <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+      <label for="" tabindex="0" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
-          <img src="https://api.lorem.space/image/face?hash=33791" />
+ 
+          <img src="https://api.lorem.space/image/face?hash=33791" alt="avatar"/>
         </div>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a href="/login">Login</a></li>
         <li>
-          <a class="justify-between">
+          <a class="justify-between" href="/profile">
             Profile
             <span class="badge">New</span>
           </a>
         </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
+        <li><a href="/settings">Settings</a></li>
+        <li><a href="/logout">Logout</a></li>
       </ul>
     </div>
       </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">Welcome to myProstate</h1>
         <p class="py-6">A home for storage and review of your prostate related health data</p>
-        <button class="btn btn-primary">Get Started</button>
+        <button class="btn btn-primary">Create Account</button>
       </div>
     </div>
   </div>
