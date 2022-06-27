7732b52052f359c15e886d1ad5519dc5ad5c5c70
conlinm <conlinm@me.com>
2022-06-27 13:24:38 -0700

    update landing page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,12 +2,17 @@
     import "/src/app.css";
     import Header from '$lib/header.svelte';
     import Footer from '../lib/footer.svelte';
+    // page is imported from the app/stores module provided by sveltekit
+    import { page } from '$app/stores';
 </script>
-
+<!-- $page.url.pathname, is reactive, and provides the route of the current page. This if conditional will prevent the header from being displayed on the root or Welcome page -->
+<div class="flex flex-col h-screen justify-between">
+{#if $page.url.pathname !== '/'}
 <Header />
-
+{/if}
 <main>
     <slot />
 </main>
 
 <Footer />
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,26 @@
       <div class="max-w-md">
         <h1 class="text-5xl font-bold">Welcome to myProstate</h1>
         <p class="py-6">A home for storage and review of your prostate related health data</p>
-        <button class="btn btn-primary">Create Account</button>
+        
+        <div class="m-4 flex flex-col 
+        items-center justify-center">
+          <form class="login space-y-2">
+            <input type="text" placeholder="myProstate Username" class="input input-bordered input-primary w-full max-w-xs" />
+            <input type="text" placeholder="Password" class="input input-bordered input-primary w-full max-w-xs" />
+            <button type="submit" formaction="/create" class="btn btn-accent w-full max-w-xs">Log In</button>
+          </form>
+        </div>
+        <div class="m-4">
+          <form >
+            <button type="submit" formaction="/create" class="btn btn-primary w-full max-w-xs">Create Account</button>
+          </form>
+        </div>
+        <div class="m-4 flex flex-col 
+        items-center justify-center space-y-2">
+          <a href="/login">Reset Password</a>
+          <a href="/login">Trouble logging in?</a>
+        </div>
+      
       </div>
     </div>
   </div>
