63c0402c4cd755a328dc437a31972bb64ab19679
conlinm <conlinm@me.com>
2022-06-28 09:47:01 -0700

    updated header and navigation

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -1,37 +1,35 @@
 <header>
-    <div class="navbar bg-secondary">
-        <div class="flex-none">
-          <!-- <div class="dropdown dropdown-right">
-          <label tabindex="0" for="" class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-          </label>
-          <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-    <li><a>Item 1</a></li>
-    <li><a>Item 2</a></li>
-  </ul>
-        </div> -->
+  <div class="navbar bg-secondary">
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl" href="/">myProstate</a>
       </div>
-        <div class="flex-1">
-          <a class="btn btn-ghost normal-case text-xl" href="/">myProstate</a>
-        </div>
-        <div class="dropdown dropdown-end">
-      <label for="" tabindex="0" class="btn btn-ghost btn-circle avatar">
-        <div class="w-10 rounded-full">
- 
-          <img src="https://api.lorem.space/image/face?hash=33791" alt="avatar"/>
-        </div>
-      </label>
-      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a href="/login">Login</a></li>
-        <li>
-          <a class="justify-between" href="/profile">
-            Profile
-            <span class="badge">New</span>
-          </a>
-        </li>
-        <li><a href="/settings">Settings</a></li>
-        <li><a href="/logout">Logout</a></li>
-      </ul>
-    </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal p-0">
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li tabindex="0">
+            <a>
+              Enter New Data
+              <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+            </a>
+            <ul class="p-2 bg-base-100">
+              <li><a href="/labentry">Lab Values</a></li>
+              <li><a href="/evententry">Clinical Event</a></li>
+            </ul>
+          </li>
+        </ul>
       </div>
+      <div class="dropdown dropdown-end">
+    <label for="" tabindex="0" class="btn btn-ghost btn-circle avatar">
+      <div class="w-10 rounded-full">
+        <img src="https://api.lorem.space/image/face?hash=33791" alt="avatar"/>
+      </div>
+    </label>
+    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+      <li><a href="/login">Login</a></li>
+      <li><a href="/profile">Profile</a></li>
+      <li><a href="/settings">Settings</a></li>
+      <li><a href="/logout">Logout</a></li>
+    </ul>
+  </div>
+</div>
 </header>
---
+++ src/routes/evententry.svelte
@@ -0,0 +1,1 @@
+<h1>Event Entry</h1>
